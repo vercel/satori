e862f53dc90f80a7e469e05dd68883731d476f2a
Shu Ding <g@shud.in>
2022-08-10 15:39:39 +0200

    Fix absolute positioning bug (#90)
    fix absolute positioning

---
+++ test/position.test.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { it, describe, expect } from 'vitest'
+
+import { initFonts } from './utils'
+import satori from '../src'
+
+describe('Position', () => {
+  let fonts
+  initFonts((f) => (fonts = f))
+
+  describe('absolute', () => {
+    it('should support absolute position', async () => {
+      const svg = await satori(
+        <div
+          style={{
+            height: '100%',
+            width: '100%',
+          }}
+        >
+          <div
+            style={{
+              position: 'absolute',
+              bottom: 0,
+              right: 0,
+              width: 10,
+              height: 10,
+              background: 'black',
+            }}
+          ></div>
+        </div>,
+        { width: 100, height: 100, fonts }
+      )
+      expect(svg).toMatchInlineSnapshot(
+        '"<svg width=\\"100\\" height=\\"100\\" viewBox=\\"0 0 100 100\\" xmlns=\\"http://www.w3.org/2000/svg\\"><rect x=\\"90\\" y=\\"90\\" width=\\"10\\" height=\\"10\\" fill=\\"black\\"/></svg>"'
+      )
+    })
+  })
+})
