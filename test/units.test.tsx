3c6ed4d8b387dbb3f3af3b59e2f3a9493358ce66
Steven <steven@ceriously.com>
2022-08-15 07:57:26 +0100

    Ignore for `display:none`

--- test/units.test.tsx
+++ test/units.test.tsx
@@ -152,7 +152,7 @@ describe('Units', () => {
     expect(result).rejects.toThrowError(`Expected <div> to have style={{display: 'flex'}} but received style={{display: 'block'}}`)
   })
 
-  it('should throw if display block on div that has children', async () => {
+  it('should throw if display inline-block on div that has children', async () => {
     const result = satori(
       <div style={{display: 'inline-block'}}>Test <span>satori</span> with space</div>,
       {
@@ -162,6 +162,18 @@ describe('Units', () => {
       }
     )
     expect(result).rejects.toThrowError(`Expected <div> to have style={{display: 'flex'}} but received style={{display: 'inline-block'}}`)
+  })
+
+  it('should not throw if display none on div that has children', async () => {
+    const svg = await satori(
+      <div style={{display: 'none'}}>Test <span>satori</span> with space</div>,
+      {
+        width: 10,
+        height: 10,
+        fonts,
+      }
+    )
+    expect(typeof svg).toBe('string');
   })
 
   it('should not throw if flex missing on span that has children', async () => {
