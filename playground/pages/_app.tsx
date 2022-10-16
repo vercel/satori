5ee2605f8e916d7c0a2da0723c09655125f87bfc
Aadarsha Acharya <aadarsha.acharya@renegadeinsurance.com>
2022-10-17 02:07:44 +0545

    feat(playground): migrate app and documents pages to typescript

--- playground/pages/_app.tsx
+++ playground/pages/_app.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import Head from 'next/head'
+import { AppProps } from 'next/app'
 
 import '../styles.css'
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps } : AppProps) {
   return (
     <>
       <Head>
