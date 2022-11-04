2d94c34687dedcc09ebe479aec32d8e4832ca825
子瞻 Luci <haru.lucinyan@gmail.com>
2022-11-05 03:57:22 +0800

    feat: throw explicit errors when useing unsupported image types (#276)
    Closes: #150

--- src/handler/image.ts
+++ src/handler/image.ts
@@ -7,6 +7,13 @@
  * TODO: We might want another option to disable image caching by default.
  */
 
+const AVIF = 'image/avif'
+const WEBP = 'image/webp'
+const PNG = 'image/png'
+const JPEG = 'image/jpeg'
+const GIF = 'image/gif'
+const SVG = 'image/svg+xml'
+
 function parseJPEG(buf: ArrayBuffer) {
   const view = new DataView(buf)
 
@@ -57,10 +64,10 @@ const cache = createLRU<ResolvedImageData>(100)
 const inflightRequests = new Map<string, Promise<ResolvedImageData>>()
 
 const ALLOWED_IMAGE_TYPES = [
-  'image/png',
-  'image/jpeg',
-  'image/gif',
-  'image/svg+xml',
+  PNG,
+  JPEG,
+  GIF,
+  SVG,
 ]
 
 function arrayBufferToBase64(buffer) {
@@ -152,30 +159,18 @@ export async function resolveImageData(
           }
         }
 
-        let imageType: string
         let imageSize: [number, number]
 
-        const magicBytes = new Uint8Array(data.slice(0, 4))
-        const magicString = [...magicBytes]
-          .map((byte) => byte.toString(16))
-          .join('')
-        switch (magicString) {
-          case '89504e47':
-            imageType = 'image/png'
+        const imageType = detectContentType(new Uint8Array(data))
+
+        switch (imageType) {
+          case PNG:
             imageSize = parsePNG(data)
             break
-          case '47494638':
-            imageType = 'image/gif'
+          case GIF:
             imageSize = parseGIF(data)
             break
-          case 'ffd8ffe0':
-          case 'ffd8ffe1':
-          case 'ffd8ffe2':
-          case 'ffd8ffe3':
-          case 'ffd8ffe8':
-          case 'ffd8ffed':
-          case 'ffd8ffdb':
-            imageType = 'image/jpeg'
+          case JPEG:
             imageSize = parseJPEG(data)
             break
         }
@@ -193,4 +188,43 @@ export async function resolveImageData(
   })
   inflightRequests.set(src, promise)
   return promise
+}
+
+/**
+ * Inspects the first few bytes of a buffer to determine if
+ * it matches the "magic number" of known file signatures.
+ * https://en.wikipedia.org/wiki/List_of_file_signatures
+ */
+function detectContentType(buffer: Uint8Array) {
+  if ([0xff, 0xd8, 0xff].every((b, i) => buffer[i] === b)) {
+    return JPEG
+  }
+  if (
+    [0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a].every(
+      (b, i) => buffer[i] === b
+    )
+  ) {
+    return PNG
+  }
+  if ([0x47, 0x49, 0x46, 0x38].every((b, i) => buffer[i] === b)) {
+    return GIF
+  }
+  if (
+    [0x52, 0x49, 0x46, 0x46, 0, 0, 0, 0, 0x57, 0x45, 0x42, 0x50].every(
+      (b, i) => !b || buffer[i] === b
+    )
+  ) {
+    return WEBP
+  }
+  if ([0x3c, 0x3f, 0x78, 0x6d, 0x6c].every((b, i) => buffer[i] === b)) {
+    return SVG
+  }
+  if (
+    [0, 0, 0, 0, 0x66, 0x74, 0x79, 0x70, 0x61, 0x76, 0x69, 0x66].every(
+      (b, i) => !b || buffer[i] === b
+    )
+  ) {
+    return AVIF
+  }
+  return null
 }
