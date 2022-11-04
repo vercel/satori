80938553c1926bc7cc3c313eb7f02bc2c8ddedfe
Jueun Grace Yun <graceyunn@gmail.com>
2022-11-03 21:03:38 -0400

    add more color tests: drop shadow and bg shorthand

--- test/color-models.test.tsx
+++ test/color-models.test.tsx
@@ -10,9 +10,337 @@ describe('Color Models', () => {
   let fonts
   initFonts((f) => (fonts = f))
 
-  // TODO: test `background` shorthand?
+  describe('background shorthand', () => {
+    it('should support hexadecimal', async () => {
+      const svg = await satori(
+        <div
+          style={{
+            display: 'flex',
+            background: '#C3D7EE',
+            color: '#132539',
+            height: '100%',
+            width: '100%',
+          }}
+        >
+          Hexadecimal
+        </div>,
+        {
+          width: 100,
+          height: 100,
+          fonts,
+        }
+      )
+      expect(toImage(svg, 100)).toMatchImageSnapshot()
+    })
 
-  // TODO: `filter` supported?
+    it('should support hexadecimal with transparency', async () => {
+      const svg = await satori(
+        <div
+          style={{
+            display: 'flex',
+            background: '#C3D7EE70',
+            color: '#13253950',
+            height: '100%',
+            width: '100%',
+          }}
+        >
+          Hexadecimal with transparency
+        </div>,
+        {
+          width: 100,
+          height: 100,
+          fonts,
+        }
+      )
+      expect(toImage(svg, 100)).toMatchImageSnapshot()
+    })
+
+    it('should support rgb', async () => {
+      const svg = await satori(
+        <div
+          style={{
+            display: 'flex',
+            background: 'rgb(148, 183, 223)',
+            color: 'rgb(19, 37, 57)',
+            height: '100%',
+            width: '100%',
+          }}
+        >
+          RGB
+        </div>,
+        {
+          width: 100,
+          height: 100,
+          fonts,
+        }
+      )
+      expect(toImage(svg, 100)).toMatchImageSnapshot()
+    })
+
+    it('should support rgba', async () => {
+      const svg = await satori(
+        <div
+          style={{
+            display: 'flex',
+            background: 'rgba(148, 183, 223, .7)',
+            color: 'rgba(19, 37, 57, .5)',
+            height: '100%',
+            width: '100%',
+          }}
+        >
+          RGBA
+        </div>,
+        {
+          width: 100,
+          height: 100,
+          fonts,
+        }
+      )
+      expect(toImage(svg, 100)).toMatchImageSnapshot()
+    })
+
+    it('should support hsl', async () => {
+      const svg = await satori(
+        <div
+          style={{
+            display: 'flex',
+            background: 'hsl(186, 22%, 26%)',
+            color: 'hsl(212, 0%, 100%)',
+            height: '100%',
+            width: '100%',
+          }}
+        >
+          HSL
+        </div>,
+        {
+          width: 100,
+          height: 100,
+          fonts,
+        }
+      )
+      expect(toImage(svg, 100)).toMatchImageSnapshot()
+    })
+
+    it('should support hsla', async () => {
+      const svg = await satori(
+        <div
+          style={{
+            display: 'flex',
+            background: 'hsla(186, 22%, 26%, .5)',
+            color: 'hsla(212, 0%, 100%, .2)',
+            height: '100%',
+            width: '100%',
+          }}
+        >
+          HSLA
+        </div>,
+        {
+          width: 100,
+          height: 100,
+          fonts,
+        }
+      )
+      expect(toImage(svg, 100)).toMatchImageSnapshot()
+    })
+
+    it('should support predefined color names', async () => {
+      const svg = await satori(
+        <div
+          style={{
+            display: 'flex',
+            background: 'pink',
+            color: 'red',
+            height: '100%',
+            width: '100%',
+          }}
+        >
+          Predefined color names
+        </div>,
+        {
+          width: 100,
+          height: 100,
+          fonts,
+        }
+      )
+      expect(toImage(svg, 100)).toMatchImageSnapshot()
+    })
+  })
+
+  describe('drop-shadow filter', () => {
+    it('should support hexadecimal', async () => {
+      const svg = await satori(
+        <div
+          style={{
+            display: 'flex',
+            backgroundColor: '#fff',
+            height: '100%',
+            width: '100%',
+          }}
+        >
+          <div style={{ filter: 'drop-shadow(16px 16px 10px #EFBA50)' }}>
+            hello world
+          </div>
+        </div>,
+        {
+          width: 100,
+          height: 100,
+          fonts,
+        }
+      )
+      expect(toImage(svg, 100)).toMatchImageSnapshot()
+    })
+
+    it('should support hexadecimal with transparency', async () => {
+      const svg = await satori(
+        <div
+          style={{
+            display: 'flex',
+            backgroundColor: '#fff',
+            height: '100%',
+            width: '100%',
+          }}
+        >
+          <div style={{ filter: 'drop-shadow(16px 16px 10px #EFBA5070)' }}>
+            hello world
+          </div>
+        </div>,
+        {
+          width: 100,
+          height: 100,
+          fonts,
+        }
+      )
+      expect(toImage(svg, 100)).toMatchImageSnapshot()
+    })
+
+    it('should support rgb', async () => {
+      const svg = await satori(
+        <div
+          style={{
+            display: 'flex',
+            background: '#fff',
+            height: '100%',
+            width: '100%',
+          }}
+        >
+          <div
+            style={{ filter: 'drop-shadow(16px 16px 10px rgb(29, 212, 84))' }}
+          >
+            hello world
+          </div>
+        </div>,
+        {
+          width: 100,
+          height: 100,
+          fonts,
+        }
+      )
+      expect(toImage(svg, 100)).toMatchImageSnapshot()
+    })
+
+    it('should support rgba', async () => {
+      const svg = await satori(
+        <div
+          style={{
+            display: 'flex',
+            background: '#fff',
+            height: '100%',
+            width: '100%',
+          }}
+        >
+          <div
+            style={{
+              filter: 'drop-shadow(16px 16px 10px rgba(29, 212, 84, .3))',
+            }}
+          >
+            hello world
+          </div>
+        </div>,
+        {
+          width: 100,
+          height: 100,
+          fonts,
+        }
+      )
+      expect(toImage(svg, 100)).toMatchImageSnapshot()
+    })
+
+    it('should support hsl', async () => {
+      const svg = await satori(
+        <div
+          style={{
+            display: 'flex',
+            background: '#fff',
+            height: '100%',
+            width: '100%',
+          }}
+        >
+          <div
+            style={{
+              filter: 'drop-shadow(16px 16px 10px hsl(268, 64%, 51%))',
+            }}
+          >
+            hello world
+          </div>
+        </div>,
+        {
+          width: 100,
+          height: 100,
+          fonts,
+        }
+      )
+      expect(toImage(svg, 100)).toMatchImageSnapshot()
+    })
+
+    it('should support hsla', async () => {
+      const svg = await satori(
+        <div
+          style={{
+            display: 'flex',
+            background: '#fff',
+            height: '100%',
+            width: '100%',
+          }}
+        >
+          <div
+            style={{
+              filter: 'drop-shadow(16px 16px 10px hsl(268, 64%, 51%, .3))',
+            }}
+          >
+            hello world
+          </div>
+        </div>,
+        {
+          width: 100,
+          height: 100,
+          fonts,
+        }
+      )
+      expect(toImage(svg, 100)).toMatchImageSnapshot()
+    })
+
+    it('should support predefined color names', async () => {
+      const svg = await satori(
+        <div
+          style={{
+            display: 'flex',
+            backgroundColor: 'pink',
+            color: 'red',
+            height: '100%',
+            width: '100%',
+          }}
+        >
+          Predefined color names
+        </div>,
+        {
+          width: 100,
+          height: 100,
+          fonts,
+        }
+      )
+      expect(toImage(svg, 100)).toMatchImageSnapshot()
+    })
+  })
 
   describe('backgroundColor and color', () => {
     it('should support hexadecimal', async () => {
@@ -194,10 +522,4 @@ describe('Color Models', () => {
     //   expect(toImage(svg, 100)).toMatchImageSnapshot()
     // })
   })
-
-  // Borders: shorthand, border-bottom-color, border-color, border-left-color, border-right-color, border-top-color
-
-  // Box shadow
-
-  // Filter
 })
