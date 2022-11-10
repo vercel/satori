24d100a051a3ea0618b1f52f2199fb84e20ed99d
luci <haru.lucinyan@gmail.com>
2022-11-10 12:12:52 +0800

    feat: support for currentcolor

--- src/utils.ts
+++ src/utils.ts
@@ -377,3 +377,7 @@ export function SVGNodeToImage(node: ReactElement): string {
     encodeURIComponent
   )}`
 }
+
+export function isString(x: unknown): x is string {
+  return typeof x === 'string'
+}
