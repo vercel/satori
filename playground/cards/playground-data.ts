e2b91e2616031116b89fbbf87daa3735a1a2ff86
Shu Ding <g@shud.in>
2023-01-06 20:40:41 +0100

    Playground improvement (#349)
    Added Turborepo and react-resizable-panels.vercel.app.

--- playground/cards/playground-data.ts
+++ playground/cards/playground-data.ts
@@ -29,62 +29,6 @@ const playgroundTabs: Tabs = {
 `,
   Vercel: `<div
   style={{
-    display: 'flex',
-    height: '100%',
-    width: '100%',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'column',
-    backgroundImage: 'linear-gradient(to bottom, #dbf4ff, #fff1f1)',
-    fontSize: 60,
-    letterSpacing: -2,
-    fontWeight: 700,
-  }}
->
-  <div
-    style={{
-      display: 'flex',
-      padding: '5px 40px',
-      width: 'auto',
-      textAlign: 'center',
-      backgroundImage: 'linear-gradient(90deg, rgb(0, 124, 240), rgb(0, 223, 216))',
-      backgroundClip: 'text',
-      '-webkit-background-clip': 'text',
-      color: 'transparent',
-    }}
-  >
-    Develop
-  </div>
-  <div
-    style={{
-      padding: '5px 40px',
-      width: 'auto',
-      textAlign: 'center',
-      backgroundImage: 'linear-gradient(90deg, rgb(121, 40, 202), rgb(255, 0, 128))',
-      backgroundClip: 'text',
-      '-webkit-background-clip': 'text',
-      color: 'transparent',
-    }}
-  >
-    Preview
-  </div>
-  <div
-    style={{
-      padding: '5px 40px',
-      width: 'auto',
-      textAlign: 'center',
-      backgroundImage: 'linear-gradient(90deg, rgb(255, 77, 77), rgb(249, 203, 40))',
-      backgroundClip: 'text',
-      '-webkit-background-clip': 'text',
-      color: 'transparent',
-    }}
-  >
-    Ship
-  </div>
-</div>
-`,
-  'Vercel Docs': `<div
-  style={{
     height: '100%',
     width: '100%',
     display: 'flex',
@@ -186,27 +130,7 @@ const playgroundTabs: Tabs = {
   </div>
 </div>
 `,
-  'Font & Emoji': `// Fallback fonts and Emoji are dynamically loaded from Google Fonts and CDNs in this demo.
-
-<div
-  style={{
-    display: 'flex',
-    height: '100%',
-    width: '100%',
-    padding: '10px 20px',
-    alignItems: 'center',
-    alignContent: 'center',
-    justifyContent: 'center',
-    fontFamily: 'Inter, "Material Icons"',
-    fontSize: 40,
-    textAlign: 'center',
-    backgroundColor: 'white',
-  }}
-  >
-  👋😄 Hello! 你好! 안녕! こんにちは! Χαίρετε! Hallå! &#xe766;
-</div>
-`,
-  'Tailwind CSS (Experimental)': `// Modified based on https://tailwindui.com/components/marketing/sections/cta-sections
+  'Tailwind (experimental)': `// Modified based on https://tailwindui.com/components/marketing/sections/cta-sections
 
 <div
   style={{
@@ -236,6 +160,53 @@ const playgroundTabs: Tabs = {
     </div>
   </div>
 </div>`,
+  Gradients: `<div
+  style={{
+    display: 'flex',
+    height: '100%',
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    backgroundImage: 'linear-gradient(to bottom, #dbf4ff, #fff1f1)',
+    fontSize: 60,
+    letterSpacing: -2,
+    fontWeight: 700,
+    textAlign: 'center',
+  }}
+  >
+  <div
+    style={{
+      backgroundImage: 'linear-gradient(90deg, rgb(0, 124, 240), rgb(0, 223, 216))',
+      backgroundClip: 'text',
+      '-webkit-background-clip': 'text',
+      color: 'transparent',
+    }}
+  >
+    Develop
+  </div>
+  <div
+    style={{
+      backgroundImage: 'linear-gradient(90deg, rgb(121, 40, 202), rgb(255, 0, 128))',
+      backgroundClip: 'text',
+      '-webkit-background-clip': 'text',
+      color: 'transparent',
+    }}
+  >
+    Preview
+  </div>
+  <div
+    style={{
+      backgroundImage: 'linear-gradient(90deg, rgb(255, 77, 77), rgb(249, 203, 40))',
+      backgroundClip: 'text',
+      '-webkit-background-clip': 'text',
+      color: 'transparent',
+    }}
+  >
+    Ship
+  </div>
+</div>
+`,
   'Color Models': `<div
   style={{
     display: 'flex',
@@ -290,6 +261,53 @@ const playgroundTabs: Tabs = {
     </div>
   </div>
 </div>`,
+  Advanced: `// Fallback fonts and Emoji are dynamically loaded
+// from Google Fonts and CDNs in this demo.
+
+// You can also return a function component in the playground.
+() => {
+function Label({ children }) {
+  return <label style={{
+    fontSize: 15,
+    fontWeight: 600,
+    textTransform: 'uppercase',
+    letterSpacing: 1,
+    margin: '20px 0 10px',
+    color: 'gray',
+  }}>
+    {children}
+  </label>
+}
+
+return (
+  <div
+    style={{
+      display: 'flex',
+      flexDirection: 'column',
+      height: '100%',
+      width: '100%',
+      padding: '10px 20px',
+      justifyContent: 'center',
+      fontFamily: 'Inter, "Material Icons"',
+      fontSize: 28,
+      backgroundColor: 'white',
+    }}
+    >
+    <Label>Language & Font subsets</Label>
+    <div>
+      Hello! 你好! 안녕! こんにちは! Χαίρετε! Hallå!
+    </div>
+    <Label>Emoji</Label>
+    <div>
+      👋 😄 🎉 🎄 🦋
+    </div>
+    <Label>Icon font</Label>
+    <div>
+        &#xe766; &#xeb9b; &#xf089;
+    </div>
+  </div>
+)
+}`,
 }
 
 export default playgroundTabs
