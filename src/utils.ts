7a8d6ed03237d1cc9c56b99506d37b02fda0e33e
Sahithyan Kandathasan <sahithyan2701@gmail.com>
2023-01-08 18:07:38 +0530

    non-breaking space (unicode) seems to work now
    not &nbsp;

--- src/utils.ts
+++ src/utils.ts
@@ -176,10 +176,34 @@ export function segment(
       granularity: 'grapheme',
     })
   }
+  
+  if (granularity === 'grapheme') {
+    return [...graphemeSegmenter.segment(content)].map((seg) => seg.segment)
+  } else {
+    const segmented = [...wordSegmenter.segment(content)].map(
+      (seg) => seg.segment
+    ) as string[]
 
-  return granularity === 'word'
-    ? [...wordSegmenter.segment(content)].map((seg) => seg.segment)
-    : [...graphemeSegmenter.segment(content)].map((seg) => seg.segment)
+    const output = []
+
+    let i = 0
+    while (i < segmented.length) {
+      const segment = segmented[i]
+
+      if (segment == '\u00a0') {
+        const previousWord = i === 0 ? '' : output.pop()
+        const nextWord = i === segmented.length - 1 ? '' : segmented[i + 1]
+
+        output.push(previousWord + '\u00a0' + nextWord)
+        i += 2
+      } else {
+        output.push(segment)
+        i++
+      }
+    }
+
+    return output
+  }
 }
 
 export function buildXMLString(
