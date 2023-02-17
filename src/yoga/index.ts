0f2a278ab34cd49365e3470a75b89028e795bdcd
Dmitry Ivakhnenko <jeetiss@yandex.ru>
2023-02-17 15:16:10 +0300

    fix yoga race

--- src/yoga/index.ts
+++ src/yoga/index.ts
@@ -4,10 +4,22 @@ export function init(yoga: typeof Yoga) {
   Yoga = yoga
 }
 
+let initializationPromise = null
+
 export default async function getYoga(): Promise<typeof Yoga> {
-  if (!Yoga) {
-    const mod = await import('@yoga')
-    Yoga = await mod.getYogaModule()
+  if (Yoga) return Yoga
+
+  if (initializationPromise) {
+    await initializationPromise
+    return Yoga
   }
+
+  initializationPromise = import('@yoga')
+    .then((mod) => mod.getYogaModule())
+    .then((yogaInstance) => (Yoga = yogaInstance))
+
+  await initializationPromise
+  initializationPromise = null
+
   return Yoga
 }
