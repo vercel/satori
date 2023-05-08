fd8dab9dca97c2925ce8da093ecc90724ff6be21
Luci <22126563+LuciNyan@users.noreply.github.com>
2023-05-08 16:55:37 +0800

    chore: add tests

---
+++ test/tab-size.test.tsx
@@ -0,0 +1,134 @@
+import { it, describe, expect } from 'vitest'
+
+import { initFonts, toImage } from './utils.js'
+import satori from '../src/index.js'
+
+describe('tab-size', () => {
+  let fonts
+  initFonts((f) => (fonts = f))
+
+  it('Tabs render correctly with default tab-size of 8', async () => {
+    const tab = String.fromCodePoint(0x09)
+    const svg = await satori(
+      <div
+        style={{
+          height: '100%',
+          width: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          backgroundColor: '#fff',
+          whiteSpace: 'pre',
+        }}
+      >
+        <div style={{ display: 'flex' }}>{tab}one tab</div>
+        <div style={{ display: 'flex', tabSize: 8 }}>{tab}one tab</div>
+        <div style={{ display: 'flex' }}>1{tab}one tab</div>
+        <div style={{ display: 'flex' }}>1111{tab}one tab</div>
+        <div style={{ display: 'flex' }}>
+          {tab}
+          {tab}two tabs
+        </div>
+        <div style={{ display: 'flex' }}>
+          1{tab}
+          {tab}two tabs
+        </div>
+        <div style={{ display: 'flex' }}>
+          1{tab}
+          {tab}
+          {tab}three tabs
+        </div>
+      </div>,
+      {
+        width: 150,
+        height: 150,
+        fonts,
+      }
+    )
+
+    expect(toImage(svg, 150)).toMatchImageSnapshot()
+  })
+
+  it('Tabs render correctly when tab-size is a number', async () => {
+    const tab = String.fromCodePoint(0x09)
+    const svg = await satori(
+      <div
+        style={{
+          height: '100%',
+          width: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          backgroundColor: '#fff',
+          whiteSpace: 'pre',
+          tabSize: 4,
+        }}
+      >
+        <div style={{ display: 'flex' }}>{tab}one tab</div>
+        <div style={{ display: 'flex', tabSize: 8 }}>{tab}one tab</div>
+        <div style={{ display: 'flex' }}>1{tab}one tab</div>
+        <div style={{ display: 'flex' }}>1111{tab}one tab</div>
+        <div style={{ display: 'flex' }}>
+          {tab}
+          {tab}two tabs
+        </div>
+        <div style={{ display: 'flex' }}>
+          1{tab}
+          {tab}two tabs
+        </div>
+        <div style={{ display: 'flex' }}>
+          1{tab}
+          {tab}
+          {tab}three tabs
+        </div>
+      </div>,
+      {
+        width: 150,
+        height: 150,
+        fonts,
+      }
+    )
+
+    expect(toImage(svg, 150)).toMatchImageSnapshot()
+  })
+
+  it('Tabs render correctly when tab-size is a string', async () => {
+    const tab = String.fromCodePoint(0x09)
+    const svg = await satori(
+      <div
+        style={{
+          height: '100%',
+          width: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          backgroundColor: '#fff',
+          whiteSpace: 'pre',
+          tabSize: '10px',
+        }}
+      >
+        <div style={{ display: 'flex' }}>{tab}one tab</div>
+        <div style={{ display: 'flex', tabSize: '1rem' }}>{tab}one tab</div>
+        <div style={{ display: 'flex' }}>1{tab}one tab</div>
+        <div style={{ display: 'flex' }}>1111{tab}one tab</div>
+        <div style={{ display: 'flex' }}>
+          {tab}
+          {tab}two tabs
+        </div>
+        <div style={{ display: 'flex' }}>
+          1{tab}
+          {tab}two tabs
+        </div>
+        <div style={{ display: 'flex' }}>
+          1{tab}
+          {tab}
+          {tab}three tabs
+        </div>
+      </div>,
+      {
+        width: 150,
+        height: 150,
+        fonts,
+      }
+    )
+
+    expect(toImage(svg, 150)).toMatchImageSnapshot()
+  })
+})
