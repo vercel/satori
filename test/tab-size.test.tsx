8299f2a8ea25b052a7b0578a33dda0e2ef179b07
Luci <22126563+LuciNyan@users.noreply.github.com>
2023-05-08 17:59:28 +0800

    chore: update tests

--- test/tab-size.test.tsx
+++ test/tab-size.test.tsx
@@ -7,7 +7,47 @@ describe('tab-size', () => {
   let fonts
   initFonts((f) => (fonts = f))
 
-  it('Tabs render correctly with default tab-size of 8', async () => {
+  it("Tab renders as space when white-space is not 'pre' or 'pre-wrap'", async () => {
+    const tab = String.fromCodePoint(0x09)
+    const svg = await satori(
+      <div
+        style={{
+          height: '100%',
+          width: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          backgroundColor: '#fff',
+        }}
+      >
+        <div style={{ display: 'flex' }}>{tab}one tab</div>
+        <div style={{ display: 'flex', tabSize: 8 }}>{tab}one tab</div>
+        <div style={{ display: 'flex' }}>1{tab}one tab</div>
+        <div style={{ display: 'flex' }}>1111{tab}one tab</div>
+        <div style={{ display: 'flex' }}>
+          {tab}
+          {tab}two tabs
+        </div>
+        <div style={{ display: 'flex' }}>
+          1{tab}
+          {tab}two tabs
+        </div>
+        <div style={{ display: 'flex' }}>
+          1{tab}
+          {tab}
+          {tab}three tabs
+        </div>
+      </div>,
+      {
+        width: 150,
+        height: 150,
+        fonts,
+      }
+    )
+
+    expect(toImage(svg, 150)).toMatchImageSnapshot()
+  })
+
+  it("Tabs render correctly with default tab-size of 8 when white-space is 'pre'", async () => {
     const tab = String.fromCodePoint(0x09)
     const svg = await satori(
       <div
@@ -106,6 +146,47 @@ describe('tab-size', () => {
       >
         <div style={{ display: 'flex' }}>{tab}one tab</div>
         <div style={{ display: 'flex', tabSize: '1rem' }}>{tab}one tab</div>
+        <div style={{ display: 'flex' }}>1{tab}one tab</div>
+        <div style={{ display: 'flex' }}>1111{tab}one tab</div>
+        <div style={{ display: 'flex' }}>
+          {tab}
+          {tab}two tabs
+        </div>
+        <div style={{ display: 'flex' }}>
+          1{tab}
+          {tab}two tabs
+        </div>
+        <div style={{ display: 'flex' }}>
+          1{tab}
+          {tab}
+          {tab}three tabs
+        </div>
+      </div>,
+      {
+        width: 150,
+        height: 150,
+        fonts,
+      }
+    )
+
+    expect(toImage(svg, 150)).toMatchImageSnapshot()
+  })
+
+  it("Tabs render correctly with default tab-size of 8 when white-space is 'pre-wrap'", async () => {
+    const tab = String.fromCodePoint(0x09)
+    const svg = await satori(
+      <div
+        style={{
+          height: '100%',
+          width: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          backgroundColor: '#fff',
+          whiteSpace: 'pre-wrap',
+        }}
+      >
+        <div style={{ display: 'flex' }}>{tab}one tab</div>
+        <div style={{ display: 'flex', tabSize: 8 }}>{tab}one tab</div>
         <div style={{ display: 'flex' }}>1{tab}one tab</div>
         <div style={{ display: 'flex' }}>1111{tab}one tab</div>
         <div style={{ display: 'flex' }}>
