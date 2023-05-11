35dc22797bc56490789ee41d6b3c2fd96bcbb126
lynn <jcignorance@outlook.com>
2023-05-11 10:58:40 +0800

    chore: update test

--- test/svg.test.tsx
+++ test/svg.test.tsx
@@ -311,67 +311,8 @@ describe('SVG', () => {
           fontWeight: 600,
         }}
       >
-        <svg
-          fill='none'
-          viewBox='0 0 180 180'
-          height={100}
-          width={100}
-          xmlns='http://www.w3.org/2000/svg'
-        >
-          <mask
-            height='180'
-            id='1'
-            maskUnits='userSpaceOnUse'
-            width='180'
-            x='0'
-            y='0'
-          >
-            <circle
-              cx='90'
-              cy='90'
-              fill='black'
-              r='90'
-              style={{ transform: 'translateX(20px)' }}
-            />
-          </mask>
-          <g mask={`url(#1)`}>
-            <circle cx='90' cy='90' fill='black' r='87' />
-            <path
-              d='M149.508 157.52L69.142 54H54V125.97H66.1136V69.3836L139.999 164.845C143.333 162.614 146.509 160.165 149.508 157.52Z'
-              fill='url(#paint0_linear_292_290)'
-            />
-            <rect
-              fill='url(#paint1_linear_292_290)'
-              height='72'
-              width='12'
-              x='115'
-              y='54'
-            />
-          </g>
-          <defs>
-            <linearGradient
-              gradientUnits='userSpaceOnUse'
-              id='paint0_linear_292_290'
-              x1='109'
-              x2='144.5'
-              y1='116.5'
-              y2='160.5'
-            >
-              <stop stopColor='white' />
-              <stop offset='1' stopColor='white' stopOpacity='0' />
-            </linearGradient>
-            <linearGradient
-              gradientUnits='userSpaceOnUse'
-              id='paint1_linear_292_290'
-              x1='121'
-              x2='120.799'
-              y1='54'
-              y2='106.875'
-            >
-              <stop stopColor='white' />
-              <stop offset='1' stopColor='white' stopOpacity='0' />
-            </linearGradient>
-          </defs>
+        <svg xmlns='http://www.w3.org/2000/svg' width='30' height='30'>
+          <circle cx='50' cy='50' r='50' style={{ fill: 'gold' }} />
         </svg>
       </div>,
       { width: 100, height: 100, fonts }
