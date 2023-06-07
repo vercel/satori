16e033422629702c0750bcfd7c8ee4592824d8e9
lynn <jcignorance@outlook.com>
2023-06-07 13:45:43 +0800

    chore: enable coverage in vitest ui

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,11 @@ import path from 'path'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
+  test: {
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+    },
+  },
   resolve: {
     alias: [
       {
