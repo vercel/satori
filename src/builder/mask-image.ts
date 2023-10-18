a00c195f077766958e895189dd6314df56791283
Paul Golmann <mail@pje-web.de>
2023-10-19 00:02:35 +0200

    fix: positioned mask image

--- src/builder/mask-image.ts
+++ src/builder/mask-image.ts
@@ -37,8 +37,8 @@ export default async function buildMaskImage(
     mask +=
       def +
       buildXMLString('rect', {
-        x: 0,
-        y: 0,
+        x: left,
+        y: top,
         width,
         height,
         fill: `url(#${_id})`,
