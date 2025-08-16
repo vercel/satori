68e5f09304b3b6cfc949665b548029ca569ef440
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2025-08-16 00:37:10 -0400

    impr(jsx): docstrings for exported types
    From React typings package.

--- src/jsx/types.ts
+++ src/jsx/types.ts
@@ -6,6 +6,21 @@
  */
 export type { CSSProperties } from './intrinsic-elements.ts'
 
+/**
+ * Represents a JSX element.
+ *
+ * Where {@link JSXNode} represents everything that can be rendered,
+ * `JSXElement` only represents JSX.
+ *
+ * @template P The type of the props object
+ * @template T The type of the component or tag
+ *
+ * @example
+ *
+ * ```tsx
+ * const element: JSXElement = <div />;
+ * ```
+ */
 export interface JSXElement<
   P = unknown,
   T extends string | FC<P> = string | FC<P>
@@ -25,7 +40,36 @@ type AwaitedJSXNode =
   | null
   | undefined
 
-// Adapted from React v19.1 `ReactNode`.
+/**
+ * Represents all of the things React can render.
+ *
+ * Where {@link JSXElement} only represents JSX, `JSXNode` represents
+ * everything that can be rendered.
+ *
+ * @see {@link https://react-typescript-cheatsheet.netlify.app/docs/react-types/reactnode/ React TypeScript Cheatsheet}
+ *
+ * @example
+ *
+ * ```tsx
+ * // Typing children
+ * type Props = { children: JSXNode }
+ *
+ * const Component = ({ children }: Props) => <div>{children}</div>
+ *
+ * <Component>hello</Component>
+ * ```
+ *
+ * @example
+ *
+ * ```tsx
+ * // Typing a custom element
+ * type Props = { customElement: JSXNode }
+ *
+ * const Component = ({ customElement }: Props) => <div>{customElement}</div>
+ *
+ * <Component customElement={<div>hello</div>} />
+ * ```
+ */
 export type JSXNode =
   | JSXElement
   | string
@@ -37,4 +81,31 @@ export type JSXNode =
   | undefined
   | Promise<AwaitedJSXNode>
 
+/**
+ * Represents the type of a function component. Can optionally receive a type
+ * argument that represents the props the component receives.
+ *
+ * @template P The props the component accepts.
+ * @see {@link https://react-typescript-cheatsheet.netlify.app/docs/basic/getting-started/function_components React TypeScript Cheatsheet}
+ *
+ * @example
+ *
+ * ```tsx
+ * // With props:
+ * type Props = { name: string }
+ *
+ * const MyComponent: FC<Props> = (props) => {
+ *  return <div>{props.name}</div>
+ * }
+ * ```
+ *
+ * @example
+ *
+ * ```tsx
+ * // Without props:
+ * const MyComponentWithoutProps: FC = () => {
+ *   return <div>MyComponentWithoutProps</div>
+ * }
+ * ```
+ */
 export type FC<P = {}> = (props: P) => JSXNode | Promise<JSXNode>
