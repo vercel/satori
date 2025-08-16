6c67af6614de08af56c13d0f7d8f246168891a77
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2025-08-15 20:07:57 -0400

    impr(jsx): `createElement()`
    More featured `createElement()` function closer to the React
    implementation.

--- src/jsx/index.ts
+++ src/jsx/index.ts
@@ -1,13 +1,31 @@
 import { jsx } from './jsx-runtime.ts'
-import type { JSXNode, FC } from './types.ts'
+import type { JSXNode, JSXElement, FC } from './types.ts'
 
 export type * from './types.ts'
 
-export function createElement(
-  type: string | FC,
-  props: Record<string, any> = {},
+/**
+ * Create a `ReactElement`-like object.
+ *
+ * @param type - Tag name string or a function component.
+ * @param props - Optional props to create the element with.
+ * @param children - Zero or more child nodes.
+ * @returns A `ReactElement`-like object with properties like `type`, `key`, `props`, and `props.children`.
+ */
+export function createElement<P extends {}>(
+  type: string | FC<P>,
+  props?: { key?: string | number | bigint | null } & Omit<P, 'children'>,
   ...children: JSXNode[]
-): JSXNode {
-  props.children = children
-  return jsx(type, props)
+): JSXElement<P> {
+  if (!props)
+    return jsx(type, children.length ? { children } : {}) as JSXElement<P>
+
+  let maybeKey: string | number | bigint | undefined | null
+  let restProps: Record<string, unknown>
+
+    // Destructure key from props.
+  ;({ key: maybeKey = null, ...restProps } = props)
+  const key = maybeKey !== null ? String(maybeKey) : null
+  if (children.length) restProps.children = children
+
+  return jsx(type, restProps, key) as JSXElement<P>
 }
