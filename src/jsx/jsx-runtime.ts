6de6cd4a0a73610a96b571560acfc7de86e4c6f6
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2025-08-16 00:39:17 -0400

    refactor(jsx): use custom IntrinsicElements
    Adapted from React typings.

--- src/jsx/jsx-runtime.ts
+++ src/jsx/jsx-runtime.ts
@@ -12,6 +12,7 @@
  * @see {@link https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/react/jsx-runtime.d.ts React typings for `jsx-runtime`}
  */
 import type { JSXElement, FC } from './types.ts'
+import type { IntrinsicElements as DefinedIntrinsixElements } from './intrinsic-elements.ts'
 
 export namespace JSX {
   /**
@@ -33,7 +34,7 @@ export namespace JSX {
   }
 
   // TODO: define IntrinsicElements supported by Satori.
-  export interface IntrinsicElements extends globalThis.JSX.IntrinsicElements {}
+  export interface IntrinsicElements extends DefinedIntrinsixElements {}
 
   export interface IntrinsicAttributes {
     /** **INFO**: Allowed as prop, but will be ignored by Satori. */
