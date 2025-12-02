7c3569bee126a2133d34ccec8b981cee72f00e3b
Deo Kumar <kumardeo080@gmail.com>
2025-12-02 10:17:21 +0530

    fix: standalone module

--- src/yoga.external.ts
+++ src/yoga.external.ts
@@ -1,34 +1,81 @@
 import { loadYoga as loadYogaUntyped, type Yoga } from 'yoga-layout/load'
 
+const loadYoga = loadYogaUntyped as (options: {
+  wasmBinary?: ArrayBuffer | ArrayBufferLike
+  instantiateWasm?: (
+    imports: WebAssembly.Imports,
+    successCallback: (instance: WebAssembly.Instance) => void
+  ) => WebAssembly.Exports | false | undefined
+}) => Promise<Yoga>
+
 let resolveYoga: (yoga: Yoga) => void
 const yogaPromise: Promise<Yoga> = new Promise((resolve) => {
   resolveYoga = resolve
 })
 
-const loadYoga = loadYogaUntyped as (
-  wasm: ArrayBuffer | ArrayBufferLike | WebAssembly.Instance
-) => Promise<Yoga>
+export type InitInput =
+  | RequestInfo
+  | URL
+  | Response
+  | BufferSource
+  | Buffer
+  | WebAssembly.Module
 
-export function init(
-  yogaWasm:
-    | ArrayBuffer
-    | ArrayBufferLike
-    | Buffer
-    | WebAssembly.Instance
-    | {
-        instance: WebAssembly.Instance
-      }
-) {
-  // Buffer
-  if ('buffer' in yogaWasm) {
-    loadYoga(yogaWasm.buffer).then(resolveYoga)
-  } else if ('instance' in yogaWasm) {
-    // { instance: WebAssembly.Instance }
-    loadYoga(yogaWasm.instance).then(resolveYoga)
-  } else {
-    // ArrayBuffer or WebAssembly.Instance
-    loadYoga(yogaWasm).then(resolveYoga)
+async function loadWasm(
+  input: InitInput,
+  imports: WebAssembly.Imports
+): Promise<WebAssembly.WebAssemblyInstantiatedSource> {
+  let source: Response | BufferSource | Buffer | WebAssembly.Module = input
+
+  if (
+    typeof source === 'string' ||
+    (typeof Request === 'function' && source instanceof Request) ||
+    (typeof URL === 'function' && source instanceof URL)
+  ) {
+    source = fetch(source)
   }
+
+  if (typeof Response === 'function' && source instanceof Response) {
+    if (typeof WebAssembly.instantiateStreaming === 'function') {
+      try {
+        return await WebAssembly.instantiateStreaming(source, imports)
+      } catch (e) {
+        if (source.headers.get('Content-Type') !== 'application/wasm') {
+          console.warn(
+            '`WebAssembly.instantiateStreaming` failed because your server does not serve Wasm with `application/wasm` MIME type. Falling back to `WebAssembly.instantiate` which is slower. Original error:\n',
+            e
+          )
+        }
+        throw e
+      }
+    }
+
+    const bytes = await source.arrayBuffer()
+    return await WebAssembly.instantiate(bytes, imports)
+  }
+
+  const instantiated = (await WebAssembly.instantiate(
+    'buffer' in source ? source.buffer : source,
+    imports
+  )) as WebAssembly.Instance | WebAssembly.WebAssemblyInstantiatedSource
+
+  if (instantiated instanceof WebAssembly.Instance) {
+    return { instance: instantiated, module: source as WebAssembly.Module }
+  }
+
+  return instantiated
+}
+
+export function init(input: InitInput) {
+  loadYoga({
+    instantiateWasm(imports, successCallback) {
+      loadWasm(input, imports).then(({ instance }) => {
+        successCallback(instance)
+      })
+
+      return {}
+    },
+  }).then(resolveYoga)
 }
 
 export function getYoga() {
--- src/yoga.ts
+++ src/yoga.ts
@@ -1,11 +1,12 @@
 import { type Yoga } from 'yoga-layout/load'
 import { type Node } from 'yoga-layout'
+import { type InitInput } from './yoga.external.js'
 
 export { Yoga as TYoga, Node as YogaNode }
 
-export function init(wasm: ArrayBuffer | Buffer) {
+export function init(input: InitInput) {
   if (process.env.SATORI_STANDALONE === '1') {
-    return import('./yoga.external.js').then((mod) => mod.init(wasm))
+    return import('./yoga.external.js').then((mod) => mod.init(input))
   } else {
     // Do nothing. It's bundled.
   }
