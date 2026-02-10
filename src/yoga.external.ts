81d02f864d4fcd8bcdba4903a6263d998dc2538d
Jiachi Liu <inbox@huozhi.im>
2026-02-10 15:24:03 +0100

    add comma
    Co-authored-by: vercel[bot] <35613825+vercel[bot]@users.noreply.github.com>

--- src/yoga.external.ts
+++ src/yoga.external.ts
@@ -58,7 +58,7 @@ async function loadWasm(
   const instantiated = (await WebAssembly.instantiate(
     'buffer' in source
       ? source.buffer.slice(source.byteOffset, source.byteOffset + source.byteLength)
-      : source
+      : source,
     imports
   )) as WebAssembly.Instance | WebAssembly.WebAssemblyInstantiatedSource
 
