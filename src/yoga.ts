987ee6350ba93867129c1365b9bd2aa0a4d78f2c
Alan Norbauer <alan@norbauer.com>
2026-04-07 00:08:43 -0700

    fix satori running in browser
    Fixes https://github.com/vercel/satori/issues/738 and restores satori's ability to run in a browser unmodified.

--- src/yoga.ts
+++ src/yoga.ts
@@ -5,7 +5,7 @@ import { type InitInput } from './yoga.external.js'
 export { Yoga as TYoga, Node as YogaNode, type InitInput }
 
 export function init(input: InitInput) {
-  if (process.env.SATORI_STANDALONE === '1') {
+  if (typeof process !== 'undefined' && process.env.SATORI_STANDALONE === '1') {
     return import('./yoga.external.js').then((mod) => mod.init(input))
   } else {
     // Do nothing. It's bundled.
@@ -13,14 +13,14 @@ export function init(input: InitInput) {
 }
 
 export function getYoga() {
-  if (process.env.SATORI_STANDALONE === '1') {
+  if (typeof process !== 'undefined' && process.env.SATORI_STANDALONE === '1') {
     return import('./yoga.external.js').then((mod) => mod.getYoga())
   } else {
     return import('./yoga.bundled.js').then((mod) => mod.getYoga())
   }
 }
 
-if (process.env.SATORI_STANDALONE !== '1') {
+if (typeof process !== 'undefined' && process.env.SATORI_STANDALONE !== '1') {
   // Preload Yoga in bundled mode.
   import('./yoga.bundled.js')
 }
