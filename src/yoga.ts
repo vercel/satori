e2bec0bf37d4ba8093106a36d8312ff3c0a1a35f
Shu Ding <g@shud.in>
2026-05-14 20:46:49 +0800

    more logs

--- src/yoga.ts
+++ src/yoga.ts
@@ -4,6 +4,36 @@ import { type InitInput } from './yoga.external.js'
 
 export { Yoga as TYoga, Node as YogaNode, type InitInput }
 
+// Unique per module-instance so two bundled copies log distinguishably.
+const BUILD_MARK =
+  '[yoga.ts id=' +
+  Math.random().toString(36).slice(2, 8) +
+  ' standalone=' +
+  String(process.env.SATORI_STANDALONE) +
+  ']'
+console.log(BUILD_MARK + ' module-load')
+
+let bundledYogaPromise: Promise<Yoga> | undefined
+
+function getBundledYoga() {
+  if (!bundledYogaPromise) {
+    console.log(BUILD_MARK + ' importing yoga.bundled')
+    const bundledYogaModulePromise = import('./yoga.bundled.js')
+    bundledYogaModulePromise.then(
+      () => console.log(BUILD_MARK + ' yoga.bundled import resolved'),
+      (err) => console.log(BUILD_MARK + ' yoga.bundled import rejected', err)
+    )
+    bundledYogaPromise = bundledYogaModulePromise.then((mod) => mod.getYoga())
+    bundledYogaPromise.then(
+      () => console.log(BUILD_MARK + ' yoga resolved'),
+      (err) => console.log(BUILD_MARK + ' yoga rejected', err)
+    )
+  } else {
+    console.log(BUILD_MARK + ' reusing yoga promise')
+  }
+  return bundledYogaPromise
+}
+
 export function init(input: InitInput) {
   if (process.env.SATORI_STANDALONE === '1') {
     return import('./yoga.external.js').then((mod) => mod.init(input))
@@ -13,14 +43,16 @@ export function init(input: InitInput) {
 }
 
 export function getYoga() {
+  console.log(BUILD_MARK + ' getYoga called')
   if (process.env.SATORI_STANDALONE === '1') {
     return import('./yoga.external.js').then((mod) => mod.getYoga())
   } else {
-    return import('./yoga.bundled.js').then((mod) => mod.getYoga())
+    return getBundledYoga()
   }
 }
 
 if (process.env.SATORI_STANDALONE !== '1') {
+  console.log(BUILD_MARK + ' kicking off preload')
   // Preload Yoga in bundled mode.
-  import('./yoga.bundled.js')
+  getBundledYoga()
 }
